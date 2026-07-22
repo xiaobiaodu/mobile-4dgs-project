96f60ab9833c80fb9c24e1164560b942d51d91eb
xiaobiaodu <10494952@qq.com>
2026-07-22 11:43:57 +0800

    add coffee steak

---
+++ render_steak/main.js
@@ -0,0 +1,7 @@
+window.FLUX_GS_CONFIG = {
+    defaultModel: "steak.json",
+    dynamicLoopSeconds: 300 / 30,
+    dynamicSortFps: 30,
+    smoothDynamicPlayback: true,
+    dynamicShDuringPlayback: false,
+};
